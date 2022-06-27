8cdd1fe4348edaf9bdefdf458736d883d1543833
AugustinRyan <80559513+augustinryan11@users.noreply.github.com>
2022-06-27 17:10:19 +0800

    Latest Code

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -27,6 +27,7 @@ $app->withFacades();
 $app->withEloquent();
 $app->configure('services');
 $app->configure('auth');
+$app->configure('database');
 
 /*
 |--------------------------------------------------------------------------
